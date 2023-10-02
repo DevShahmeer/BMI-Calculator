3b9031e372bd9acc50afb1d042e9fe059574b75e
shahmeer <theshahmeerfsd@gmail.com>
2023-10-02 23:57:49 +0500

    remove some bang and improve code

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,11 +1,12 @@
-import * as readline from 'readline';
-
+// Define a function to calculate BMI
 function calculateBMI(height: number, weight: number): number {
-  const heightInMeters = height / 100;
+  // BMI formula: weight (kg) / (height (m))^2
+  const heightInMeters = height / 100; // Convert height to meters
   const bmi = weight / (heightInMeters * heightInMeters);
   return parseFloat(bmi.toFixed(2));
 }
 
+// Function to categorize BMI
 function getBMICategory(bmi: number): string {
   if (bmi < 18.5) {
     return 'Underweight';
@@ -18,19 +19,23 @@ function getBMICategory(bmi: number): string {
   }
 }
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-function getUserInput(): Promise<{ height: number; weight: number }> {
-  return new Promise((resolve, reject) => {
-    rl.question('Enter your height in cm: ', (height) => {
-      rl.question('Enter your weight in kg: ', (weight) => {
-        resolve({ height: parseFloat(height), weight: parseFloat(weight) });
-        rl.close();
-      });
-    });
-  });
+// Function to get user input
+function getUserInput(): { height: number; weight: number } {
+  const height = parseFloat(prompt('Enter your height in cm: ')); // Prompt for height
+  const weight = parseFloat(prompt('Enter your weight in kg: ')); // Prompt for weight
+  return { height, weight };
 }
 
+// Main function to calculate BMI and display the results
+function calculateAndDisplayBMI(): void {
+  const { height, weight } = getUserInput(); // Get user input
+  const bmi = calculateBMI(height, weight); // Calculate BMI
+  const category = getBMICategory(bmi); // Determine BMI category
+
+  // Display results
+  console.log('BMI:', bmi);
+  console.log('Category:', category);
+}
+
+// Run the calculation and display
+calculateAndDisplayBMI();
