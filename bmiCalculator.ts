c471243fa77e2e38dcf37e2ade42226056b6a4a2
shahmeer <theshahmeerfsd@gmail.com>
2023-10-03 00:11:28 +0500

    Basic structure and BMI calculation

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,12 +1,11 @@
-// Define a function to calculate BMI
+import * as readline from 'readline';
+
 function calculateBMI(height: number, weight: number): number {
-  // BMI formula: weight (kg) / (height (m))^2
-  const heightInMeters = height / 100; // Convert height to meters
+  const heightInMeters = height / 100;
   const bmi = weight / (heightInMeters * heightInMeters);
   return parseFloat(bmi.toFixed(2));
 }
 
-// Function to categorize BMI
 function getBMICategory(bmi: number): string {
   if (bmi < 18.5) {
     return 'Underweight';
@@ -19,23 +18,29 @@ function getBMICategory(bmi: number): string {
   }
 }
 
-// Function to get user input
-function getUserInput(): { height: number; weight: number } {
-  const height = parseFloat(prompt('Enter your height in cm: ')); // Prompt for height
-  const weight = parseFloat(prompt('Enter your weight in kg: ')); // Prompt for weight
-  return { height, weight };
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function getUserInput(): Promise<{ height: number; weight: number }> {
+  return new Promise((resolve, reject) => {
+    rl.question('Enter your height in cm: ', (height) => {
+      rl.question('Enter your weight in kg: ', (weight) => {
+        resolve({ height: parseFloat(height), weight: parseFloat(weight) });
+        rl.close();
+      });
+    });
+  });
 }
 
-// Main function to calculate BMI and display the results
-function calculateAndDisplayBMI(): void {
-  const { height, weight } = getUserInput(); // Get user input
-  const bmi = calculateBMI(height, weight); // Calculate BMI
-  const category = getBMICategory(bmi); // Determine BMI category
+async function calculateAndDisplayBMI(): Promise<void> {
+  const { height, weight } = await getUserInput();
+  const bmi = calculateBMI(height, weight);
+  const category = getBMICategory(bmi);
 
-  // Display results
   console.log('BMI:', bmi);
   console.log('Category:', category);
 }
 
-// Run the calculation and display
 calculateAndDisplayBMI();
